ee794e3b6a35cfcd0cc67a64a9d958f11d0c861e
yongryang <chesidyong@gmail.com>
2020-01-21 09:24:43 +0900

    Add live-visit-count

---
+++ 31Days31Projects/live-visit-count/app.js
@@ -0,0 +1,10 @@
+const countEl = document.getElementById('count')
+updateVisitCount();
+
+function updateVisitCount() {
+    fetch('https://api.countapi.xyz/update/yongjunleeme/youtube/?amount=1')
+    .then(res => res.json())
+        .then(res => {
+            countEl.innerHTML = res.value; 
+        })
+}
